0af7a45b2e0254c2d09fb7a4a956b92340909bf2
nguyenbob94 <93953690+nguyenbob94@users.noreply.github.com>
2022-08-31 23:08:07 +1000

    Update array-and-wildcardfilter.py

--- array-and-wildcardfilter.py
+++ array-and-wildcardfilter.py
@@ -2,6 +2,7 @@ import re
 
 airports = ["YMML", "YSSY", "YPPH", "VVTS", "EGLL", "EGKK", "KLAX"]
 
+#Return only Australian airports (Beginning with Y)
 for i in airports:
   if re.search('Y.', i):
     print(i)
